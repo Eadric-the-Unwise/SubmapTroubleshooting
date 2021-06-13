4d153e4add9a576f57bc33bca695ca7de5580a5e
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2021-06-13 20:04:29 +0900

    submaps_working!

--- large_map.c
+++ large_map.c
@@ -14,9 +14,13 @@ UINT16 camera_max_x;
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 
 UBYTE joy;
+UBYTE indoor;
+UBYTE sliding = 0;
+INT8 slide_x = 0;
+INT8 slide_y = 0;
 
 // current and old positions of the camera in pixels
-UWORD camera_x, camera_y, old_camera_x, old_camera_y;
+WORD camera_x, camera_y, old_camera_x, old_camera_y;
 // current and old position of the map in tiles
 UBYTE map_pos_x, map_pos_y, old_map_pos_x, old_map_pos_y;
 // redraw flag, indicates that camera position was changed
@@ -92,6 +96,7 @@ void init_map()
     set_level(bigmap_mapWidth, bigmap_mapHeight, bigmap_map);
 
     redraw = FALSE;
+    indoor = 0;
 
     SCX_REG = camera_x;
     SCY_REG = camera_y;
@@ -118,6 +123,7 @@ void init_house()
     old_camera_y = camera_y;
 
     redraw = FALSE;
+    indoor = 1;
 
     SCX_REG = camera_x;
     SCY_REG = camera_y;
@@ -127,7 +133,7 @@ void init_house()
 void init_submap()
 {
     HIDE_BKG;
-    camera_x = 320;
+    camera_x = 160;
     camera_y = 144;
     old_camera_x = camera_x;
     old_camera_y = camera_y;
@@ -145,6 +151,7 @@ void init_submap()
     old_camera_y = camera_y;
 
     redraw = FALSE;
+    indoor = 0;
 
     SCX_REG = camera_x;
     SCY_REG = camera_y;
@@ -162,49 +169,137 @@ void main()
     while (TRUE)
     {
         joy = joypad();
-        if (joy & J_START)
-        {
-            init_house();
-        }
+        // Add above loop (beginning of else-if loop, effectively)
         if (joy & J_SELECT)
         {
             init_submap();
         }
-
-        // up or down
-        if (joy & J_UP)
+        else if (joy & J_START)
         {
-            if (camera_y)
+            init_house();
+        }
+        if (indoor)
+        {
+            if (joy & J_UP)
             {
-                camera_y--;
-                redraw = TRUE;
+                if (camera_y)
+                {
+                    camera_y--;
+                    redraw = TRUE;
+                }
+            }
+            if (joy & J_DOWN)
+            {
+                if (camera_y < camera_max_y)
+                {
+                    camera_y++;
+                    redraw = TRUE;
+                }
+            }
+        }
+
+        else if (sliding)
+        {
+            // If the camera and slide is inside the map, slide, otherwise cancel slide
+            if (!(camera_x + slide_x < 0 || camera_x + slide_x > camera_max_x ||
+                  camera_y + slide_y < 0 || camera_y + slide_y > camera_max_y))
+            {
+                camera_x += slide_x; // Move as much as slide in X direction
+                camera_y += slide_y; // " " in Y direction
+                redraw = TRUE;       // Flag for redraw
+            }
+            else
+                sliding = FALSE;
+
+            // If camera is at the end of the slide, stop sliding
+            if (camera_x % 160 == 0 && camera_y % 144 == 0)
+                sliding = FALSE;
+        }
+        // make sure else if before all other inputs
+        else if (joy & J_LEFT)
+        {
+            // Realistically this would be gated by an edge-check and valid-edge check
+            // (this would replace camera_x++/-- or camera_y++/--)
+            slide_x = -2; // How much to move in the X direction
+            slide_y = 0;  // How much to move in the Y direction
+            sliding = 1;  // Initiate slide
+        }
+        else if (joy & J_RIGHT)
+        {
+            slide_x = 2;
+            slide_y = 0;
+            sliding = 1;
+        }
+        else if (joy & J_UP)
+        {
+            slide_x = 0;
+            slide_y = -2;
+            sliding = 1;
+            if (indoor)
+            {
+                if (camera_y)
+                {
+                    camera_y--;
+                    redraw = TRUE;
+                }
             }
         }
         else if (joy & J_DOWN)
         {
-            if (camera_y < camera_max_y)
+            slide_x = 0;
+            slide_y = 2;
+            sliding = 1;
+            if (indoor)
             {
-                camera_y++;
-                redraw = TRUE;
+                if (camera_y)
+                {
+                    camera_y++;
+                    redraw = TRUE;
+                }
             }
         }
-        // left or right
-        if (joy & J_LEFT)
-        {
-            if (camera_x)
-            {
-                camera_x--;
-                redraw = TRUE;
-            }
-        }
-        else if (joy & J_RIGHT)
-        {
-            if (camera_x < camera_max_x)
-            {
-                camera_x++;
-                redraw = TRUE;
-            }
-        }
+
+        // // up or down
+        // if (joy & J_UP)
+        // {
+        //     if (camera_y)
+        //     {
+        //         camera_y--;
+        //         redraw = TRUE;
+        //     }
+        // }
+        // else if (joy & J_DOWN)
+        // {
+        //     if (camera_y < camera_max_y)
+        //     {
+        //         camera_y++;
+        //         redraw = TRUE;
+        //     }
+        // }
+        // // left or right
+        // if (joy & J_LEFT)
+        // {
+        //     if (indoor == 0)
+        //     {
+        //         if (camera_x)
+        //         {
+        //             camera_x--;
+        //             redraw = TRUE;
+        //         }
+        //     }
+
+        // }
+        // else if (joy & J_RIGHT)
+        // {
+        //     if (indoor == 0)
+        //     {
+        //         if (camera_x < camera_max_x)
+        //         {
+        //             camera_x++;
+        //             redraw = TRUE;
+        //         }
+        //     }
+        // }
         if (redraw)
         {
             wait_vbl_done();
